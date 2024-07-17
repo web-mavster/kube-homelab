e5ff5447b1f09e6a0268fb5f27f6833245e8c884
Miguel Vargas Ink <miguel.vargas@inkaviation.com>
2024-07-16 23:39:44 -0500

    feat: Add kernel module configuration for Kubernetes resources
    The code changes in this commit add and remove kernel module configurations for Kubernetes resources. This improves the stability and performance of the Kubernetes cluster.

--- molecules/kubernetes/resource.tf
+++ molecules/kubernetes/resource.tf
@@ -106,23 +106,8 @@ resource "ssh_resource" "enable_kernel_modules" {
     "sudo modprobe iptable_filter",
     "sudo modprobe iptable_nat",
     "sudo modprobe nf_nat",
-    "sudo modprobe nf_conntrack_ipv4",
-    "sudo modprobe nf_conntrack_ipv6",
-    "sudo modprobe nf_nat_ipv4",
-    "sudo modprobe nf_nat_ipv6",
-    "sudo modprobe nf_conntrack_netlink",
-    "sudo modprobe nf_defrag_ipv4",
-    "sudo modprobe nf_defrag_ipv6",
     "sudo modprobe nf_tables",
-    "sudo modprobe nf_tables_inet",
-    "sudo modprobe nf_tables_ipv4",
-    "sudo modprobe nf_tables_ipv6",
     "sudo modprobe nfnetlink",
-    "sudo modprobe nfnetlink_queue",
-    "sudo modprobe nfnetlink_log",
-    "sudo modprobe nfnetlink_cthelper",
-    "sudo modprobe nfnetlink_cttimeout",
-    "sudo modprobe nf_conntrack_netlink"
   ]
   private_key = file(each.value.private_key)
   timeout = "10m"
@@ -143,23 +128,8 @@ resource "ssh_resource" "disable_kernel_modules" {
     "sudo modprobe -r iptable_filter",
     "sudo modprobe -r iptable_nat",
     "sudo modprobe -r nf_nat",
-    "sudo modprobe -r nf_conntrack_ipv4",
-    "sudo modprobe -r nf_conntrack_ipv6",
-    "sudo modprobe -r nf_nat_ipv4",
-    "sudo modprobe -r nf_nat_ipv6",
-    "sudo modprobe -r nf_conntrack_netlink",
-    "sudo modprobe -r nf_defrag_ipv4",
-    "sudo modprobe -r nf_defrag_ipv6",
     "sudo modprobe -r nf_tables",
-    "sudo modprobe -r nf_tables_inet",
-    "sudo modprobe -r nf_tables_ipv4",
-    "sudo modprobe -r nf_tables_ipv6",
     "sudo modprobe -r nfnetlink",
-    "sudo modprobe -r nfnetlink_queue",
-    "sudo modprobe -r nfnetlink_log",
-    "sudo modprobe -r nfnetlink_cthelper",
-    "sudo modprobe -r nfnetlink_cttimeout",
-    "sudo modprobe -r nf_conntrack_netlink"
   ]
   private_key = file(each.value.private_key)
   timeout = "10m"
@@ -173,10 +143,10 @@ resource "ssh_resource" "kernel_boot" {
   user     = each.value.user
   commands = [
     "sudo cp /etc/sysctl.conf /etc/sysctl.conf.bak",
-    "sudo echo 'net.bridge.bridge-nf-call-iptables = 1' >> /etc/sysctl.conf",
-    "sudo echo 'net.bridge.bridge-nf-call-ip6tables = 1' >> /etc/sysctl.conf",
-    "sudo echo 'net.ipv4.ip_forward = 1' >> /etc/sysctl.conf",
-    "sudo echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.conf",
+    "net.bridge.bridge-nf-call-iptables = 1 | sudo tee -a /etc/sysctl.conf",
+    "net.bridge.bridge-nf-call-ip6tables = 1 | sudo tee -a /etc/sysctl.conf",
+    "net.ipv4.ip_forward = 1 | sudo tee -a /etc/sysctl.conf",
+    "net.ipv6.conf.all.forwarding = 1 | sudo tee -a /etc/sysctl.conf",
     "sudo sysctl -p",
     "sudo cp /boot/firmware/cmdline.txt /boot/firmware/cmdline.txt.bak",
     "sudo sed -i 's/$/ cgroup_enable=cpuset cgroup_enable=memory cgroup_memory=1/' /boot/firmware/cmdline.txt"
--- molecules/update/resource.tf
+++ molecules/update/resource.tf
@@ -10,3 +10,136 @@ resource "ssh_resource" "update_dependencies" {
   private_key = file(each.value.private_key)
   timeout = "10m"
 }
+
+# Enable eth0 and disable wlan0
+resource "ssh_resource" "enable_eth0_disable_wlan0" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  commands = [
+    "sudo ip link set eth0 up",
+    "sudo ip link set wlan0 down"
+  ]
+  private_key = file(each.value.private_key)
+  timeout = "10m"
+}
+
+# Disable eth0 and enable wlan0
+resource "ssh_resource" "disable_eth0_enable_wlan0" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  commands = [
+    "sudo ip link set eth0 down",
+    "sudo ip link set wlan0 up"
+  ]
+  private_key = file(each.value.private_key)
+  timeout = "10m"
+  when = "destroy"
+}
+
+# /etc/dhcpcd.conf
+resource "ssh_resource" "set_static_ip_dhcpcd" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  commands = [
+    "sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak",
+    "echo 'interface eth0' | sudo tee -a /etc/dhcpcd.conf",
+    "echo 'static ip_address=$(hostname -I | awk '{print $1}')/24' | sudo tee -a /etc/dhcpcd.conf",
+    "echo 'static routers=$(ip route | awk '/default/ { print $3 }' | sort | uniq | head -n 1)' | sudo tee -a /etc/dhcpcd.conf",
+    "echo 'static domain_name_servers=$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}')' | sudo tee -a /etc/dhcpcd.conf"
+  ]
+  private_key = file(each.value.private_key)
+  timeout = "10m"
+}
+
+# Restart dhcpcd service
+resource "ssh_resource" "restart_dhcpcd" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  commands = [
+    "sudo mv /etc/dhcpcd.conf.bak /etc/dhcpcd.conf",
+    "sudo rm -rf /etc/dhcpcd.conf.bak",
+    "sudo systemctl daemon-reload",
+    "sudo systemctl restart dhcpcd"
+  ]
+  private_key = file(each.value.private_key)
+  timeout = "10m"
+  when = "destroy"
+}
+
+resource "ssh_resource" "set_static_ip" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  file {
+    content  = <<-EOT
+      network:
+        version: 2
+        renderer: networkd
+        wifis:
+          renderer: networkd
+          wlan0:
+            access-points:
+              NAZGUL_REY_PLUS:
+                password: 02f94c39b9132a19d4c7375860e9010aeaa0f31beddc905ff89964a513f3d998
+            dhcp4: true
+            optional: true
+        ethernets:
+          eth0:
+            addresses:
+              - IP_ADDRESS/24
+            gateway4: IP_GATEWAY
+            nameservers:
+              addresses:
+                - DNS1
+            optional: true
+            dhcp4: false
+      EOT
+    destination = "/etc/netplan/01-netcfg.yaml"
+    permissions = "0644"
+  }
+  commands = [
+    "sudo set -i 's/IP_ADDRESS/'$(hostname -I | awk '{print $1}')'/g' /etc/netplan/01-netcfg.yaml",
+    "sudo set -i 's/IP_GATEWAY/'$(ip route | awk '/default/ { print $3 }' | sort | uniq | head -n 1)'/g' /etc/netplan/01-netcfg.yaml",
+    "sudo set -i 's/DNS1/'$(grep 'nameserver' /etc/resolv.conf | awk '{print $2}')'/g' /etc/netplan/01-netcfg.yaml",
+    "sudo netplan --debug try",
+    "sudo netplan --debug generate",
+    "sudo netplan --debug apply",
+  ]
+}
+
+resource "ssh_resource" "set_dhcp_ip" {
+  for_each = local.servers
+  host     = each.value.host
+  user     = each.value.user
+  file {
+    content  = <<-EOT
+      network:
+        version: 2
+        renderer: networkd
+        wifis:
+          renderer: networkd
+          wlan0:
+            access-points:
+              NAZGUL_REY_PLUS:
+                password: 02f94c39b9132a19d4c7375860e9010aeaa0f31beddc905ff89964a513f3d998
+            dhcp4: true
+            optional: true
+        ethernets:
+          eth0:
+            dhcp4: true
+            optional: true
+      EOT
+    destination = "/etc/netplan/01-netcfg.yaml"
+    permissions = "0644"
+  }
+  commands = [
+    "sudo netplan --debug try",
+    "sudo netplan --debug generate",
+    "sudo netplan --debug apply",
+  ]
+  when = "destroy"
+}
